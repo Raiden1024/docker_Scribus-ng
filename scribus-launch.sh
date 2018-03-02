5a644e73a4a7fc4107322382d0ad88687ff0aef0
Raiden1024 <34368228+Raiden1024@users.noreply.github.com>
2018-03-02 14:29:01 +0100

    Update scribus-launch.sh

--- scribus-launch.sh
+++ scribus-launch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-USER_NAME=`basename $VHOME`
-HOME_NAME=`dirname $VHOME`
+USERNAME=`basename $VHOME`
+HOMENAME=`dirname $VHOME`
 
-USER_ID=`ls -lahn $HOME_NAME | grep $USER_NAME | awk {'print $3'}`
-GROUP_ID=`ls -lahn $HOME_NAME | grep $USER_NAME | awk {'print $4'}`
+USERID=`ls -lahn $HOME_NAME | grep $USERNAME | awk {'print $3'}`
+GROUPID=`ls -lahn $HOME_NAME | grep $USERNAME | awk {'print $4'}`
 
-groupadd -g $GROUP_ID scribus
-useradd --shell /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
-su $USER_NAME -c "/usr/bin/scribus-ng"
+groupadd -g $GROUPID scribus
+useradd --shell /bin/bash --uid $USERID --gid $GROUPID $USERNAME
+su $USERNAME -c "/usr/bin/scribus-ng"
